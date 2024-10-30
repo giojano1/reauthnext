2ef2fc34f3c59fdd67610e302045dceaae998f32
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2024-10-30 21:52:46 +0400

    adding login page

---
+++ app/login/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FormProvider } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { passwordSchema } from "@/validation/passwordSchema";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: passwordSchema,
+});
+
+export default function RegisterPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  //   console.log(form);
+
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register for a new account.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* Email Field */}
+          <FormProvider {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="gap-5 flex flex-col"
+            >
+              <fieldset
+                className="gap-5 flex flex-col"
+                disabled={form.formState.isSubmitting}
+              >
+                {/* Email Field */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Password Field */}
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="password"
+                          {...field}
+                          type="password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Login</Button>
+              </fieldset>
+            </form>
+          </FormProvider>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
