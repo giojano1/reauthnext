4d5cea7a6f9dc1cb28c97934c7ef83eed397fddb
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2024-11-04 16:36:56 +0400

    adding change password

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,9 +1,18 @@
 // თუ მომხამრებელი არის ავტორიზირებული, არ შეუშვა შიგნით არსებულ როუთებზე.
 
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 export default async function LoggedOutLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!!session?.user?.id) {
+    redirect("/my-account");
+  }
+
   return children;
 }
